9b08e8400aaa5dbf7fc3524c92a8ddb98bdf984e
Jonathas-Ramos <139417020+Jonathas-Ramos@users.noreply.github.com>
2025-10-30 01:17:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // --- VARIÁVEIS GLOBAIS ---
-    const API_URL = 'https://taskflowup2.onrender.com/api';;
+    const API_URL = 'https://taskflowup2.onrender.com/api';
     let currentUser = null;
     let allTasks = [];
     let currentFilter = 'all';
@@ -1275,4 +1275,5 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     
 });
+
 
